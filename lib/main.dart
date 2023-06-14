7884e700dc24ff3aacf42bd5f8d8b3c17ff80240
a17876 <a17876@hanmail.net>
2023-06-13 20:44:52 -0700

    Container and apply theme in home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pomodoro/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         cardColor: const Color(0xFFF4EDDB),
       ),
       home: const Scaffold(
-        body: Center(),
+        body: HomeScreen(),
       ),
     );
   }
